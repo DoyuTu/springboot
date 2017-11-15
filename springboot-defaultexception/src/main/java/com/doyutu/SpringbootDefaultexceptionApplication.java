59bc897c9e4de453d178cadd6df5eb6d0fb943e3
doyutu <859898972@qq.com>
2017-11-15 16:24:59 +0800

    Initial commit

---
+++ springboot-defaultexception/src/main/java/com/doyutu/SpringbootDefaultexceptionApplication.java
@@ -0,0 +1,12 @@
+package com.doyutu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDefaultexceptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDefaultexceptionApplication.class, args);
+	}
+}
