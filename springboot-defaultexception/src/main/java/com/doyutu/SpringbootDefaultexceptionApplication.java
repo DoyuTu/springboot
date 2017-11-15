e1b3339a0f4306092cdcb34aa3710ce869a755e7
doyutu <859898972@qq.com>
2017-11-15 20:32:58 +0800

    commit

--- springboot-defaultexception/src/main/java/com/doyutu/SpringbootDefaultexceptionApplication.java
+++ springboot-defaultexception/src/main/java/com/doyutu/SpringbootDefaultexceptionApplication.java
@@ -2,11 +2,27 @@ package com.doyutu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.web.servlet.ErrorPage;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.HttpStatus;
 
 @SpringBootApplication
-@ComponentScan
+@ComponentScan(basePackages = "com.doyutu.**")
 public class SpringbootDefaultexceptionApplication {
+
+	@Bean
+	public EmbeddedServletContainerCustomizer containerCustomizer() {
+
+		return (container -> {
+			ErrorPage error401Page = new ErrorPage(HttpStatus.UNAUTHORIZED, "/401.html");
+			ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/404.html");
+			ErrorPage error500Page = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/404.html");
+
+			container.addErrorPages(error401Page, error404Page, error500Page);
+		});
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootDefaultexceptionApplication.class, args);
