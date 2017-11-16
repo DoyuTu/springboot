a7ee6c38e11e1b0c04d60c902f4697e702a1d6d5
doyutu <859898972@qq.com>
2017-11-16 16:09:28 +0800

    AsyncRestTemplate

--- springboot-async/src/main/java/com/doyutu/SpringbootAsyncApplication.java
+++ springboot-async/src/main/java/com/doyutu/SpringbootAsyncApplication.java
@@ -17,8 +17,8 @@ public class SpringbootAsyncApplication {
 	public AsyncRestTemplate asyncRestTemplate() {
 		AsyncRestTemplate asyncRestTemplate = new AsyncRestTemplate();
 		/** 设置编码器为UTF-8 防止中文乱码，其RestTemplate默认编码为 ISO-8859-1
-		 * @see org.springframework.web.client.RestTemplate
-		 * @see org.springframework.http.converter.StringHttpMessageConverter.DEFAULT_CHARSET
+		 * {@link org.springframework.web.client.RestTemplate}
+		 * {@link org.springframework.http.converter.StringHttpMessageConverter.DEFAULT_CHARSET}
 		 */
 		asyncRestTemplate.getMessageConverters().set(1, new StringHttpMessageConverter(StandardCharsets.UTF_8));
 		return asyncRestTemplate;
