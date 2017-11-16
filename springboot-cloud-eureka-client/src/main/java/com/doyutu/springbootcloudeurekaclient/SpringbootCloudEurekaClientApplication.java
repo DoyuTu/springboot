98abb3ec961e45fe0f6192fd9c1cc8c59ba07162
doyutu <859898972@qq.com>
2017-11-17 00:01:44 +0800

    springboot-cloud-eureka-client

---
+++ springboot-cloud-eureka-client/src/main/java/com/doyutu/springbootcloudeurekaclient/SpringbootCloudEurekaClientApplication.java
@@ -0,0 +1,14 @@
+package com.doyutu.springbootcloudeurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SpringbootCloudEurekaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCloudEurekaClientApplication.class, args);
+	}
+}
---
+++ springboot-cloud-eureka-client/src/main/java/com/doyutu/springbootcloudeurekaclient/controller/DcController.java
@@ -0,0 +1,21 @@
+package com.doyutu.springbootcloudeurekaclient.controller;
+
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+public class DcController {
+
+    @Resource
+    private DiscoveryClient discoveryClient;
+
+    @GetMapping("/dc")
+    public String dc() {
+        String services = "Services: " + discoveryClient.getServices();
+        System.out.println(services);
+        return services;
+    }
+}
