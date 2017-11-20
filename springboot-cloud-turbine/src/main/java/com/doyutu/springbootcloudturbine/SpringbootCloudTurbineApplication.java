ff0e257c4e1197d0056292f259ce0e814c80b3cb
doyutu <859898972@qq.com>
2017-11-20 17:36:06 +0800

    springboot-cloud-turbine Support

---
+++ springboot-cloud-turbine/src/main/java/com/doyutu/springbootcloudturbine/SpringbootCloudTurbineApplication.java
@@ -0,0 +1,14 @@
+package com.doyutu.springbootcloudturbine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+@SpringBootApplication
+@EnableTurbine
+public class SpringbootCloudTurbineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCloudTurbineApplication.class, args);
+	}
+}
