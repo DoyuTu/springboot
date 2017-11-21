343780e6fc2c00ae24bf41710591efe730f89b24
doyutu <859898972@qq.com>
2017-11-21 16:28:53 +0800

    schedule

---
+++ springboot-schedule/src/main/java/com/doyutu/springbootschedule/SpringbootScheduleApplication.java
@@ -0,0 +1,12 @@
+package com.doyutu.springbootschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootScheduleApplication.class, args);
+	}
+}
