15bc297845b3277e169c2f4791cb6eba41a712f6
doyutu <859898972@qq.com>
2017-11-29 18:32:44 +0800

    bus

---
+++ springboot-cloud-bus/src/main/java/com/doyutu/springbootcloudbus/SpringbootCloudBusApplication.java
@@ -0,0 +1,12 @@
+package com.doyutu.springbootcloudbus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCloudBusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCloudBusApplication.class, args);
+	}
+}
