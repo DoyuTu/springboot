cb83e4715ac168bb7d24574be7427ab8e6f756b4
doyutu <859898972@qq.com>
2018-02-10 11:41:46 +0800

    mybatis plus

--- springboot-mybatis/src/main/java/com/doyutu/springbootmybatis/domain/SuperEntity.java
+++ springboot-mybatis/src/main/java/com/doyutu/springbootmybatis/domain/SuperEntity.java
@@ -4,10 +4,12 @@ package com.doyutu.springbootmybatis.domain;
 import com.baomidou.mybatisplus.activerecord.Model;
 import com.baomidou.mybatisplus.annotations.TableId;
 import com.baomidou.mybatisplus.enums.IdType;
+import lombok.Data;
 
 import java.io.Serializable;
 
 @SuppressWarnings("serial")
+@Data
 public class SuperEntity<T extends Model> extends Model<T> {
 
     /**
@@ -16,22 +18,6 @@ public class SuperEntity<T extends Model> extends Model<T> {
     @TableId(value = "test_id", type = IdType.ID_WORKER)
     private Long id;
     private Long tenantId;
-
-    public Long getId() {
-        return this.id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getTenantId() {
-        return tenantId;
-    }
-
-    public void setTenantId(Long tenantId) {
-        this.tenantId = tenantId;
-    }
 
     @Override
     protected Serializable pkVal() {
--- springboot-mybatis/src/main/java/com/doyutu/springbootmybatis/domain/User.java
+++ springboot-mybatis/src/main/java/com/doyutu/springbootmybatis/domain/User.java
@@ -4,10 +4,16 @@ import com.baomidou.mybatisplus.annotations.TableField;
 import com.baomidou.mybatisplus.annotations.TableLogic;
 import com.doyutu.springbootmybatis.domain.enums.AgeEnum;
 import com.doyutu.springbootmybatis.domain.enums.PhoneEnum;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 @SuppressWarnings("serial")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User extends SuperEntity<User> {
 
     /**
@@ -30,9 +36,6 @@ public class User extends SuperEntity<User> {
     private Long role;
     private PhoneEnum phone;
 
-    public User() {
-    }
-
     public User(Long id, String name, AgeEnum age, Integer testType) {
         this.setId(id);
         this.name = name;
@@ -46,59 +49,4 @@ public class User extends SuperEntity<User> {
         this.testType = testType;
     }
 
-
-    public String getName() {
-        return this.name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public AgeEnum getAge() {
-        return this.age;
-    }
-
-    public void setAge(AgeEnum age) {
-        this.age = age;
-    }
-
-    public Integer getTestType() {
-        return this.testType;
-    }
-
-    public void setTestType(Integer testType) {
-        this.testType = testType;
-    }
-
-    public Long getRole() {
-        return this.role;
-    }
-
-    public void setRole(Long role) {
-        this.role = role;
-    }
-
-    public PhoneEnum getPhone() {
-        return this.phone;
-    }
-
-    public void setPhone(PhoneEnum phone) {
-        this.phone = phone;
-    }
-
-    public Date getTestDate() {
-        return testDate;
-    }
-
-    public void setTestDate(Date testDate) {
-        this.testDate = testDate;
-    }
-
-    @Override
-    public String toString() {
-        return "User [id=" + this.getId() + ", name=" + name + ", age=" + age
-                + ", testType=" + testType + ", testDate="
-                + testDate + ", role=" + role + ", phone=" + phone + "]";
-    }
 }
