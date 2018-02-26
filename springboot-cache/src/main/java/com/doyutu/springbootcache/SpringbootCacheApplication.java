c8d897d413fa7696d2fe96f981a3bcc063a823d9
doyutu <859898972@qq.com>
2018-02-27 00:04:44 +0800

    pom fix

--- springboot-cache/src/main/java/com/doyutu/springbootcache/SpringbootCacheApplication.java
+++ springboot-cache/src/main/java/com/doyutu/springbootcache/SpringbootCacheApplication.java
@@ -2,9 +2,18 @@ package com.doyutu.springbootcache;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 @SpringBootApplication
+@Configuration
 public class SpringbootCacheApplication {
+
+    @Bean
+    public StringRedisTemplate stringRedisTemplate() {
+        return new StringRedisTemplate();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootCacheApplication.class, args);
--- springboot-cache/src/main/java/com/doyutu/springbootcache/redis/RedisCache.java
+++ springboot-cache/src/main/java/com/doyutu/springbootcache/redis/RedisCache.java
@@ -3,9 +3,10 @@ package com.doyutu.springbootcache.redis;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -15,13 +16,14 @@ public class RedisCache {
     private static final Logger logger = LoggerFactory.getLogger(RedisCache.class);
 
     @Autowired
-    private RedisTemplate<String, Object> redisTemplate;
+    @Qualifier(value = "stringRedisTemplate")
+    private StringRedisTemplate redisTemplate;
 
-    public final String WORD = "world hello!";
+    final String WORD = "world hello!";
 
     public String redis() {
         redisTemplate.opsForValue().set("test", WORD);
-        return redisTemplate.opsForValue().get("test").toString();
+        return redisTemplate.opsForValue().get("test");
     }
 
     /**
