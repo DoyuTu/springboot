de8f46a499b74ed4154b1c40f1cc6f4a5e056c2b
doyutu <859898972@qq.com>
2018-04-20 03:42:10 +0800

    init aop

--- springboot-aop/src/main/java/com/doyutu/springbootaop/proxy/CglibProxy.java
+++ springboot-aop/src/main/java/com/doyutu/springbootaop/proxy/CglibProxy.java
@@ -1,9 +1,9 @@
 package com.doyutu.springbootaop.proxy;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.cglib.proxy.Enhancer;
-import org.springframework.cglib.proxy.MethodInterceptor;
-import org.springframework.cglib.proxy.MethodProxy;
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
 
 import java.lang.reflect.Method;
 import java.util.Map;
@@ -20,23 +20,19 @@ public class CglibProxy implements MethodInterceptor {
 
     private static final Map<Method, Object> interceptMap = new ConcurrentHashMap<>();
 
-    Enhancer enhancer = new Enhancer();
-
     public Object getProxy(Class<?> cls) {
         Enhancer enhancer = new Enhancer();
-        enhancer.setCallback(this);
         enhancer.setSuperclass(cls);
+        enhancer.setCallback(this);
         return enhancer.create();
     }
 
     @Override
     public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
-        log.info("enter proxy");
         Object invokeSuper = methodProxy.invokeSuper(o, objects);
-//        if (!interceptMap.containsKey(method)) {
-//            return invokeSuper;
-//        }
-        log.info("leave proxy");
+        if (!interceptMap.containsKey(method)) {
+            return invokeSuper;
+        }
         return invokeSuper;
     }
 
